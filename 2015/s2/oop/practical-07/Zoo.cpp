96d60c652eed633c2a24ec68e64c5ed1f28eac66
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2015-09-10 09:46:31 +0000

    third

---
+++ 2015/s2/oop/practical-07/Zoo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "Animal.h"
+#include "Wolf.h"
+#include "Dragon.h"
+#include "Zoo.h"
+using namespace std;
+
+
+
+void Zoo:add(Dragon* vect1[],Wolf* vect2[],int dnum, int wnum){
+int i=0;
+int j=0;
+       
+	for(i=0;i<dnum;i++){
+	vec1[i]=vect1[i];
+}
+for(j=0;j<wnum;j++){
+
+	vec2[i]=vect2[i];
+}
+}
+
+void zoo::print
+
+Zoo::Zoo(Dragon* v1[],Wolf* v2[],string zookeeper, int ldrag,int lwolf){
+   
+        add(v1,v2,ldrag,lwolf); //running the add function
+
+}
+
---
+++ 2015/s2/oop/practical-07/Zoo.h
@@ -0,0 +1,19 @@
+#include <string>
+#include "Animal.h"
+#include "Wolf.h"
+#include "Dragon.h"
+
+class Zo{ 
+
+public:
+   
+    Zoo(Dragon* v1[],Wolf* v2[],zookeeper,int ldrag,int lwolf);  //constructors
+   
+    Dragon* vec1[];  //states
+	Wolf* vec2[];
+	std::string zookeeper;
+   
+    void add(Dragon* vect1[],Wolf* vect2[], int dnum, int wnum);  //behaviours
+};
+
+
