48750df396e3d3b915398bea3635b49cf10bcc2f
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-03-06 04:46:42 +0000

    adding design

---
+++ 2016/s1/adds/assignment1/Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Palindrome{
+
+	private:
+
+	protected:
+
+	public:
+
+	};
+
+Palindrome::Palindrome(things to take in string){
+	functions
+	}
+
+define functions and what they do
