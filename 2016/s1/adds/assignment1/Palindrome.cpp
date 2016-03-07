ef79ddd9a6cfbebe21e9190ba655da2db27c7e5c
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-03-07 02:27:29 +0000

    working at uni

--- 2016/s1/adds/assignment1/Palindrome.cpp
+++ 2016/s1/adds/assignment1/Palindrome.cpp
@@ -1,19 +1,117 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 class Palindrome{
 
 	private:
-
-	protected:
+		
+		string enteredPhrase;
+		string allLettersPhrase;
+		string lowerCasePhrase;
 
 	public:
 
-	};
+		Palindrome();
 
-Palindrome::Palindrome(things to take in string){
-	functions
+		void setEnteredPhrase(string es);
+		void setAllLettersPhrase(string ls);
+		void setLowerCasePhrase(string lcs);
+		
+		string removeNonLetters(string s1);
+		string lowerCase(string s2);
+		int isPalindrome(string s3);
+
+};
+
+Palindrome::Palindrome(){
+
+};
+
+void Palindrome::setEnteredPhrase(string es){
+	enteredPhrase=es;
+};
+
+void Palindrome::setAllLettersPhrase(string ls){
+	allLettersPhrase=ls;
+};
+
+void Palindrome::setLowerCasePhrase(string lcs){
+	lowerCasePhrase=lcs;
+};
+		
+string Palindrome::removeNonLetters(string s1){
+	string newS1;
+	int i=0;
+	int j=0;
+	for(i=0;i<strlen(s1);i++){ //change to while loop and end at null
+		if((int)s1[i]>64 && (int)s1[i]<91){
+			newS1[j]=s1[i];
+			j++;
+		}
+		else if((int)s1[i]>96 && (int)s1[i]<123){
+			newS1[j]=s1[i];
+			j++;
+		}
+		else{
+		}
 	}
+	newS1[j]='\0';
+	return newS1;
+};
 
-define functions and what they do
+string Palindrome::lowerCase(string s2){
+	string newS2=tolower(s2);
+	return newS2;
+};
+
+int Palindrome::isPalindrome(string s3){
+	int x=strlen(s3)-1;
+	int y=x;
+	int z=0;
+	if(x%2==0){
+		for(int i=0;i<(x/2);i++){
+			if(s3[i]==s3[y]){
+				z++;
+				y--;
+			}
+			else{
+				break;
+			}
+		}
+		if(z==x/2){
+			return 1;
+		else{
+			return 0;
+		}
+	}
+	else{
+		for(int i=0;i<((x/2)+1);i++){
+			if(s3[i]==s3[y]){
+				z++;
+				y--;
+			}
+			else{
+				break;
+			}
+		}
+		if(z==((x/2)+1)){
+			return 1;
+		else{
+			return 0;
+		}
+	}	
+};
+
+
+
+
+
+
+
+
+
+
--- 2016/s1/adds/assignment1/main.cpp
+++ 2016/s1/adds/assignment1/main.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
+
+int main(){
+	string phrase;
+	cout<<"Please enter a string to test if a palindrome: ";
+	getline(cin,phrase);
+	Palindrome palindrome();
+	palindrome.setEnteredPhrase(phrase);
+	palindrome.setAllLettersPhrase(palindrome.removeNonLetters(enteredPhrase));
+	palindrome.setLowerCasePhrase(palindrome.lowerCase(allLettersPhrase));
+	if(palindrome.isPalindrome(lowerCasePhrase)==1){
+		cout<<"Yes"<<endl;
+	}
+	else{
+		cout<<"No"<<endl;
+	}
+
+};
