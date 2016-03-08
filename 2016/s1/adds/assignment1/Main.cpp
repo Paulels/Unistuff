a3509f5d278dc9e4cb043a072598ffa86156aebc
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-03-08 01:47:58 +0000

    another shot

---
+++ 2016/s1/adds/assignment1/Main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stdio.h>
+#include "Palindrome.h"
+using namespace std;
+
+int main(){
+	string phrase;
+	cout<<"Please enter a string to test if a palindrome: ";
+	getline(cin,phrase);
+	Palindrome Palindrome;
+	Palindrome.setEnteredPhrase(phrase);
+	string str1=Palindrome.getEnteredPhrase();
+	Palindrome.setAllLettersPhrase(Palindrome.removeNonLetters(str1));
+	string str2=Palindrome.getAllLettersPhrase();
+	Palindrome.setLowerCasePhrase(Palindrome.lowerCase(str2));
+	string str3=Palindrome.getLowerCasePhrase();
+	if(Palindrome.isPalindrome(str3)==1){
+		cout<<"Yes"<<endl;
+	}
+	else{
+		cout<<"No"<<endl;
+	}
+
+};
