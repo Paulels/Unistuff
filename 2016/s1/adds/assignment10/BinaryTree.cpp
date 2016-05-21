e56641dc0a454cbc7afff5e009385781ec547814
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-05-21 05:30:44 +0000

    added BT.cpp

---
+++ 2016/s1/adds/assignment10/BinaryTree.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <stdio.h>
+#include <ctype.h>
+#include <cstdlib>
+#include <vector>
+#include <math.h>
+#include "BinaryTree.h"
+using namespace std;
+
+//Just a basic constructor setting the variables to 0
+BinaryTree::BinaryTree(){
+
+	root=NULL;
+}
+
+//A deconstructor that destroys all the nodes in the linked list so ther is no memory leaks 
+BinaryTree::~BinaryTree(){
+
+	while(head!=NULL){					//?????????????????????????????????
+		Node* old=head;
+		head=head->getNext();
+		delete old;
+	}
+}
+
+//returns the position of the first bit
+Node* BinaryTree::getRoot(){
+
+	return root;
+};
+
+//This will add a node to the tail with a value of val. It has O(1) complexity
+void BinaryTree::addNode(char ch){
+
+
+};
+
+//
+void BinaryTree::printList(){
+
+
+};
+
+
+
