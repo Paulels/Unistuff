06d2106d11be5ed7c012e2e04fc5fe72a6bb4629
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-08-18 05:27:38 +0000

    asm code

---
+++ 2016/s2/cs/add10.asm
@@ -0,0 +1,30 @@
+	@sum
+	M=0 //sum==0
+	@i
+	M=0 //i=0
+	@10
+	D=A //D=10
+	@counter
+	M=D //counter==10
+	@array
+	D=M //D==array
+(LOOP)
+	@sum
+	M=M+D
+	@counter
+	M=M-1
+	D=M
+	@END
+	D;JLE
+	@array
+	D=A
+	@i
+	M=M+1
+	D=D+M
+	@array
+M=D
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
