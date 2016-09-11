9bd1a6d839d629cb9e36f194fa609f481e89f3a5
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-09-11 10:49:35 +0000

    part of asmsymbols done

--- 2016/s2/cs/assignment3/asmsymbols.cpp
+++ 2016/s2/cs/assignment3/asmsymbols.cpp
@@ -2,22 +2,36 @@
 
 #include "asmsymbols.h"
 #include <iostream>
+#include <map>
 
 using namespace std ;
 
 // define your own class to implement symbols_int
 class symbols_int_x : public symbols_int
 {
+	map<string,int> SiTable;
+
         bool insert(string symbol,int value)		// returns false for duplicate symbol
 	{
-		return false ;
+		if(SiTable.count(symbol)==0){
+			SiTable.insert(pair<string,int>(symbol,value));
+		}
+		else{
+			return false;
+		}
 	}
         int lookup(string symbol)			// returns -1 for undefined symbol
 	{
-		return -1 ;
+		if(SiTable.count(symbol)==0){
+			return -1;
+		}
+		else{
+			return SiTable.at(symbol);
+		}
 	}
 	void display()					// display contents in an arbitrary order
 	{
+
 	}
 	void deleteme()					// deletes the symbol table
 	{
@@ -32,14 +46,26 @@ symbols_int *symbols_int::newtable()
 // define your own class to implement symbols_string
 class symbols_string_x : public symbols_string
 {
+	map<string,string> SsTable;
+
 	// remember to include definitions of all the inherited pure virtual functions
         bool insert(string symbol,string value)		// returns false for duplicate symbol
 	{
-		return false ;
+		if(SsTable.count(symbol)==0){
+			SsTable.insert(pair<string,string>(symbol,value));
+		}
+		else{
+			return false;
+		}
 	}
         string lookup(string symbol)			// returns "" for undefined symbol
 	{
-		return "" ;
+		if(SsTable.count(symbol)==0){
+			return "";
+		}
+		else{
+			return SsTable.at(symbol);
+		}
 	}
 	void display()					// display contents in an arbitrary order
 	{
