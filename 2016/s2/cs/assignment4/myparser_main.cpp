817c23e8fa2eacfd6259ec980fbcb2577e563f73
a1686498 <a1686498@b861a905-1c34-4a56-bf3e-af31ebc04eab>
2016-10-20 01:44:45 +0000

    doing

--- 2016/s2/cs/assignment4/myparser_main.cpp
+++ 2016/s2/cs/assignment4/myparser_main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+#include <stdlib.h>
+#include "myParser.h"
+#include "jacktokens.h"
+
+using namespace std ;
+
+
 int main()
 {
+	// create a tokeniser and read first token to initialise it
+	jacktokens *tokeniser = jacktokens::newtokeniser() ;
+	token = tokeniser->next_token() ;
+
+
+	
+
+	// parse a Program
+	parseProgram() ;
+
+	// check for end of file
+	mustbe("?") ;
+
+	return 0;
 }
